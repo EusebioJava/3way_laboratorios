d7fb928525dd89b29507f02f5c622e0a9b2f3f42
javasegquasex <javasegquasex@GEDAY>
2017-10-19 21:04:17 -0200

    Inicio do Projeto do Projeto com git ignore

---
+++ Projeto_calculadoras/src/br/com/threeway/Main.java
@@ -0,0 +1,11 @@
+package br.com.threeway;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testando a calculadora");
+		
+	}
+
+}
